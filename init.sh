9042b9b737b31c6bdb795907e11eeee41d56c963
lucaspdfborges <lucaspdfborges@gmail.com>
2019-02-13 13:32:03 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,12 @@
 echo 'Defaults        env_keep = "http_proxy ftp_proxy"' | sudo EDITOR='tee -a' visudo
 gnome-terminal --command='ping 10.66.10.68'
 
+#enable access to repos
+echo "deb-src http://archive.ubuntu.com/ubuntu bionic main restricted" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu/ bionic main restricted multiverse universe" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://security.ubuntu.com/ubuntu/ bionic-security main restricted multiverse universe" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted multiverse universe" | sudo tee -a /etc/apt/sources.list
+
 # setting up django
 sudo apt-get update
 sudo apt-get install python3-pip
@@ -14,3 +20,5 @@ sudo apt-get install atom
 atom sensorweb
 cd sensorweb
 source myvenv/bin/activate
+
+
