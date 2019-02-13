2da14dfec85df9c329ebcb2ac44909dcec8131ab
lucaspdfborges <lucaspdfborges@gmail.com>
2019-02-13 15:20:16 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,15 +13,22 @@ echo "deb-src http://security.ubuntu.com/ubuntu/ bionic-security main restricted
 echo "deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted multiverse universe  #Added by software-properties
 " | sudo tee -a /etc/apt/sources.list
 
+sleep 5
+
 # setting up django
 sudo apt-get update
-sudo apt-get install python3-pip
+sudo apt-get install -y python3-pip
 pip3 install django
 cd Desktop
-git clone https://github.com/lucaspdfborges/sensorweb.git -u lucaspdfborges
+sudo apt-get install -y git
+git clone https://github.com/lucaspdfborges/sensorweb.git
 
 # Downloading and launching atom
-sudo apt-get install atom
+cd ..
+cd Downloads
+wget -O atom.deb "https://atom.io/download/deb"
+sudo dpkg -i atom.deb
+sudo apt-get install -f
 atom sensorweb
 cd sensorweb
 source myvenv/bin/activate
