aa64c65a5e16354ca57587460f78d9c756aec0cf
lucaspdfborges <lucaspdfborges@gmail.com>
2019-02-13 12:43:49 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #setting up internet connection
-echo 'Defaults        env_keep = "http_proxy ftp_proxy"' | sudo EDITOR='tee -a'$
+echo 'Defaults        env_keep = "http_proxy ftp_proxy"' | sudo EDITOR='tee -a' visudo
 gnome-terminal --command='ping 10.66.10.68'
 
 # setting up django
@@ -9,6 +9,6 @@ pip3 install django
 
 # Downloading atom
 sudo apt-get install -y curl
-curl https://atom.io/download/deb --output atom.deb
-sudo dpkg -i atom.deb
+curl https://atom.io/download/deb --output atom-amd64.deb
+sudo dpkg -i atom-amd64.deb
 sudo apt-get install -f
