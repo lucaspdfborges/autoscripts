e7cc22e3822fd224bb749f1a07acb0d281047564
lucaspdfborges <lucaspdfborges@gmail.com>
2019-02-13 13:36:28 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,10 +3,14 @@ echo 'Defaults        env_keep = "http_proxy ftp_proxy"' | sudo EDITOR='tee -a' 
 gnome-terminal --command='ping 10.66.10.68'
 
 #enable access to repos
-echo "deb-src http://archive.ubuntu.com/ubuntu bionic main restricted" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://archive.ubuntu.com/ubuntu/ bionic main restricted multiverse universe" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://security.ubuntu.com/ubuntu/ bionic-security main restricted multiverse universe" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted multiverse universe" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu bionic main restricted  #Added by software-properties
+" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu/ bionic main restricted multiverse universe  #Added by software-properties
+" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://security.ubuntu.com/ubuntu/ bionic-security main restricted multiverse universe #Added by software-properties
+ " | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted multiverse universe  #Added by software-properties
+" | sudo tee -a /etc/apt/sources.list
 
 # setting up django
 sudo apt-get update
