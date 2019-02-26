0dffc0781bbbb253e06197598de2e5e49ba0b539
lucaspdfborges <lucaspdfborges@gmail.com>
2019-02-26 12:34:32 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -49,6 +49,12 @@ sudo apt-get update && sudo apt-get install yarn
 
 https://github.com/glenpike/npm-g_nosudo
 
+# Install MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
 
 # Starting the webapp
 cd ..
