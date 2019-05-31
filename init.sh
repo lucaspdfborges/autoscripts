9f1d71cc19aed0fd550fd60ccc55c87258e41715
Lucas Borges <lucaspdfborges@gmail.com>
2019-05-31 11:43:05 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,22 +1,9 @@
 #setting up internet connection
 echo 'Defaults        env_keep = "http_proxy ftp_proxy"' | sudo EDITOR='tee -a' visudo
-gnome-terminal --command='ping 10.66.10.68'
-
-#enable access to repos
-echo "deb-src http://archive.ubuntu.com/ubuntu bionic main restricted  #Added by software-properties
-" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://archive.ubuntu.com/ubuntu/ bionic main restricted multiverse universe  #Added by software-properties
-" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://security.ubuntu.com/ubuntu/ bionic-security main restricted multiverse universe #Added by software-properties
- " | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted multiverse universe  #Added by software-properties
-" | sudo tee -a /etc/apt/sources.list
-
-sleep 5
-
-# setting up django
 sudo rm /etc/apt/sources.list
 sudo apt-get update
+
+
 sudo apt-get install -y python3-pip
 pip3 install django
 cd Desktop
